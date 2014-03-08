bd4c89e03fdb0780223484e94652d9280f210633
Skriz <magdalena.skrizevska@yahoo.com>
2014-03-08 21:48:13 +0100

    Update Edge.java

--- src/teamwork/linkpred_matrix/Edge.java
+++ src/teamwork/linkpred_matrix/Edge.java
@@ -1,6 +1,25 @@
 package teamwork.linkpred_matrix;
 
 public class Edge {
-	
+	double weight;
+	double [] features;
+
+    public Edge() {
+        this.weight = 0;
+        this.features = null;
+    }
+
+    public Edge( double[] features) {
+        this.weight = 0;
+        this.features = features;
+    }
+
+    public void setFeatures(double[] features) {
+        this.features = features;
+    }
+    
+    
+        
+        
 
 }
