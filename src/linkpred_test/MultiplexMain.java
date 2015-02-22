8addc9b71a74299379bb3d0090000511e3256c5e
Verica Lazova <vericalcroft@gmail.com>
2015-02-22 20:48:49 +0100

    Bug fix within multiplexNetwork

--- src/linkpred_test/MultiplexMain.java
+++ src/linkpred_test/MultiplexMain.java
@@ -23,9 +23,10 @@ public class MultiplexMain {
 	public static void main(String[] args) {
 		System.out.println("Graph generation start");           
 		
-		int n = 100;                                            // number of nodes per graph   
+		int n = 1000;                                           // number of nodes per graph   
 		int f1 = 2;                                             // number of features for the first graph
-		int f2 = 3;                                             // numer of features for the second graph
+		int f2 = 3;                                             // number of features for the second graph
+		int f3 = 2;                                             // number of features for the third graph
 		
 		int s = 0;                                              // the node whose links we learn, in this case 0 for each graph
 		double alpha = 0.0;                                     // we use no damping factor within the multiplex, interlayer jump instead
@@ -33,23 +34,19 @@ public class MultiplexMain {
 		double b = 1e-6;                                        // WMW function parameter
 		double lambda = 1;                                      // regularization parameter 
 		
-		double [] param = {1, -1, 0.5, -2, 1};                  // parameters vector 2 for the first graph, 3 for the second
+		double [] param = {1, -1, 0.5, -2, 1, -2, 2};           // parameters vector 2 for the first graph, 3 for the second
 		DoubleMatrix1D parameters = new 
 				DenseDoubleMatrix1D(param);	
-		DoubleMatrix1D parameters1 = new 
-				DenseDoubleMatrix1D(new double [] {1, -1});	
-		DoubleMatrix1D parameters2 = new 
-				DenseDoubleMatrix1D(new double [] {0.5, -2, 1});	
-				
+						
 		int topN = 10; 
-		
-		
-		
-		Network [] graphs = new Network [2];                    // build the graph
+			
+		Network [] graphs = new Network [3];                    // build the graph
 		ArtificialGraphGenerator.initialize(f1);
 		graphs[0] = (Network) ArtificialGraphGenerator.generate(n, f1, s);
 		ArtificialGraphGenerator.initialize(f2);
 		graphs[1] = (Network) ArtificialGraphGenerator.generate(n, f2, s);
+		ArtificialGraphGenerator.initialize(f3);
+		graphs[2] = (Network) ArtificialGraphGenerator.generate(n, f3, s); 
 		
 		MultiplexNetwork multiplex = new MultiplexNetwork(graphs, interlayer);
 		ArtificialGraphGenerator.buildDandL(multiplex, topN, parameters, alpha);
@@ -65,17 +62,18 @@ public class MultiplexMain {
 		
 		// GRADIENT DESCENT OPTIMIZATION START
 		
-		int maxIterations = 500;
-		int restarts = 20;
+		int maxIterations = 150;
+		int restarts = 10;
 		double gradientTreshold = 1e-5;
 		double costThreshold = 5.6;
-		double [] initialParameters = new double [f1 + f2];
-		for (int i = 0; i < f1 + f2; i++)
+		int f = f1+f2+f3;
+		double [] initialParameters = new double [f];
+		for (int i = 0; i < f; i++)
 			initialParameters[i] = Math.random() * 2 - 1;                 // select random value between -1 and 1 instead between 0 and 1   
 		
 		
 		GradientDescent gd = new GradientDescent(new LinkPredictionTrainer(
-				new RandomWalkGraph [] {multiplex}, f1+f2, alpha, lambda, b, 0.0003), // TODO 
+				new RandomWalkGraph [] {multiplex}, f, alpha, lambda, b, 0.0003), // TODO 
 				maxIterations, 
 				gradientTreshold, 
 				costThreshold);
@@ -100,21 +98,22 @@ public class MultiplexMain {
 		
 		System.out.println(gd.getStopReason());
 		System.out.println("Function minimum: " + optimum.getValue() + "\nParameters: ");
-		for (int i = 0; i < f1+f2; i++)
+		for (int i = 0; i < f; i++)
 		        System.out.print(optimum.getPoint()[i] + " ");
 		System.out.println("\nResults in in " + (end-start)/60E9 + " minutes.");
 		
-		
-		
+				
 		// PREDICTIONS
 		ArrayList<Integer> trueLinks = new ArrayList<Integer>();
 		ArrayList<Integer> predictedLinks = new ArrayList<Integer>();
 		
-		Network [] testGraphs = new Network [2];                    // build the graph
+		Network [] testGraphs = new Network [3];                    // build the graph
 		ArtificialGraphGenerator.initialize(f1);
 		testGraphs[0] = (Network) ArtificialGraphGenerator.generate(n, f1, s);
 		ArtificialGraphGenerator.initialize(f2);
 		testGraphs[1] = (Network) ArtificialGraphGenerator.generate(n, f2, s);
+		ArtificialGraphGenerator.initialize(f3);
+		testGraphs[2] = (Network) ArtificialGraphGenerator.generate(n, f3, s);
 		
 		MultiplexNetwork testMultiplex = new MultiplexNetwork(graphs, interlayer);
 		
@@ -128,7 +127,7 @@ public class MultiplexMain {
 		
 		System.out.println("\nPredicted links:");
 		for (int i = 0; i < predictedLinks.size(); i++)
-			System.out.print(predictedLinks.get(i) + "(" + predictedLinks.get(i) % n + ") ");
+			System.out.print(predictedLinks.get(i) + " ");
 		System.out.println();
 		
 	}
